8939e08c0d5c3ec28841de0f91aa7cb976b6caef
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2016-04-23 12:36:28 +0100

    Added installation for git and updating macports

--- install_script.sh
+++ install_script.sh
@@ -1,4 +1,4 @@
-echo 'Beginning installation........'
+echo 'Beginning installation... \n\n'
 
 # Going firstly in Downloads folder and doing all our dirty stuff
 
@@ -10,6 +10,8 @@ mkdir temp_folder
 cd temp_folder
 
 # Install MacPorts
+
+echo 'Installing MacPorts... \n\n'
 
 curl -O https://distfiles.macports.org/MacPorts/MacPorts-2.3.3.tar.bz2
 tar xf MacPorts-2.3.3.tar.bz2
@@ -23,12 +25,16 @@ rm -rf Macports-2.3.3
 
 # Update MacPorts
 
+echo 'Updating MacPorts to latest version... \n\n"
 sudo port selfupdate
+echo 'Update complete!\n\n'
 
 # Install git
 
+echo 'Installing git... \n\n'
 sudo port install git-core
+echo 'Installation complete!\n\n'
 
-# Install Vim
+# Install MacVim
 
-
+echo 'Installing MacVim... \n\n' 
